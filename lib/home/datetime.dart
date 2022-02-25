475f3e69ae6cfc661bd119975d481bf53d161605
Jahongir Anvarov <95640875+JaxaAnvarov@users.noreply.github.com>
2022-02-25 14:43:52 +0500

    ,,

--- lib/home/datetime.dart
+++ lib/home/datetime.dart
@@ -18,37 +18,41 @@ class _DateTimeeState extends State<DateTimee> {
         title: const Text('Work With DateTime'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          const Text(
-            'Date Time',
-            style: TextStyle(
-                color: Colors.indigo,
-                fontSize: 25.0,
-                fontWeight: FontWeight.w500),
-          ),
-          ElevatedButton(
-            // Display a CupertinoDatePicker in dateTime picker mode.
-            onPressed: () => _showDialog(
-              CupertinoDatePicker(
-                initialDateTime: dateTime,
-                use24hFormat: true,
-                // This is called when the user changes the dateTime.
-                onDateTimeChanged: (DateTime newDateTime) {
-                  setState(() => dateTime = newDateTime);
-                },
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Date Time',
+              style: TextStyle(
+                  color: Colors.indigo,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w500),
+            ),
+            ElevatedButton(
+              // Display a CupertinoDatePicker in dateTime picker mode.
+              onPressed: () => _showDialog(
+                CupertinoDatePicker(
+                  initialDateTime: dateTime,
+                  use24hFormat: true,
+                  // This is called when the user changes the dateTime.
+                  onDateTimeChanged: (DateTime newDateTime) {
+                    setState(() => dateTime = newDateTime);
+                  },
+                ),
+              ),
+              // In this example, time value is formatted manually. You can use intl package to
+              // format the value based on the user's locale settings.
+              child: Text(
+                '${dateTime.month}-${dateTime.day}-${dateTime.year} ${dateTime.hour}:${dateTime.minute}',
+                style: const TextStyle(
+                  fontSize: 22.0,
+                ),
               ),
             ),
-            // In this example, time value is formatted manually. You can use intl package to
-            // format the value based on the user's locale settings.
-            child: Text(
-              '${dateTime.month}-${dateTime.day}-${dateTime.year} ${dateTime.hour}:${dateTime.minute}',
-              style: const TextStyle(
-                fontSize: 22.0,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
